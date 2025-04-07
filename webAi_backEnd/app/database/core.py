21b8ed1202e21d3b66a695f347a0992c2da71478
jqjiksina <2271835419@qq.com>
2025-04-07 21:14:39 +0800

    file restored

--- webAi_backEnd/app/database/core.py
+++ webAi_backEnd/app/database/core.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-from database.models import Base
+from database.models import BaseModel, Group
 
 # 从环境变量获取连接信息
-DATABASE_URL = "mysql+asyncmy://root:password@localhost:3306/mydb"
+DATABASE_URL = "mysql+asyncmy://root:password@localhost:3306/mydb?charset=utf8mb4"
 
 # 数据库异步引擎
 async_engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
@@ -21,15 +21,22 @@ AsyncSessionLocal = async_sessionmaker(async_engine, class_=AsyncSession, expire
 async def recreate_tables():
     print("recreating table...")
     async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(BaseModel.metadata.drop_all)
+        await conn.run_sync(BaseModel.metadata.create_all)
+    async with AsyncSessionLocal() as session:
+        print("creating initial group...")
+        group = Group(name = "base_group", assistant_id = "86f13f7410b311f0aa470242ac130006")
+        session.add(group)
+        await session.commit()
+        
+async def create_tables():
+    print('creating table...')
+    async with async_engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
         
 #初始化数据库表结构
 async def init_db() :
-    print('creating table...')
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    # await recreate_tables()
+    await create_tables()
 
 # 获取异步会话的依赖
 async def get_async_db():
