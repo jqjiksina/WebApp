b184036fc41da8af9347518d553b2e952730c05f
jqjiksina <2271835419@qq.com>
2025-04-09 18:22:56 +0800

    set on linux

---
+++ launch_linux.sh
@@ -0,0 +1,23 @@
+conda activate webAi
+
+cd ragflow/docker
+
+docker compose up -d
+
+cd ../../
+
+:: cd ../../ollama
+
+:: docker-compose up -d % 启动ollama %
+
+cd webAi_backEnd
+
+docker-compose up -d % 启动mysql %
+
+:: python webAi_backEnd/app/main.py
+
+conda deactivate
+
+:: cd webAi_frontEnd
+
+:: npm run dev
